d13ced43dbcfa99f1d85b8481247df5d52c08712
Rich Barusta <ricardo.barusta@sw.com.mx>
2017-02-21 16:57:23 -0600

    Change base path dev env to unit test

--- src/main/java/Utils/Constants.java
+++ src/main/java/Utils/Constants.java
@@ -1,7 +1,7 @@
 package Utils;
 
 public class Constants {
-    public static String BASE_PATH = "http://swservicestest.azurewebsites.net";
+    public static String BASE_PATH = "http://services.test.sw.com.mx";
     public static String AUTH_PATH = "/security/authenticate";
     public static String STAMP_PATH = "/cfdi33/stamp/";
     public static String auth_soap_envelope(String user, String pwd){

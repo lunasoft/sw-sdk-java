8705af3c7dedaabbc7fc61ad7820c6dfde648d36
Rich Barusta <ricardo.barusta@gmail.com>
2017-05-15 11:17:53 -0500

    Revert "Release/sw.sdk java(0.0.2.2)"

--- src/main/java/Utils/Requests/IRequest.java
+++ src/main/java/Utils/Requests/IRequest.java
@@ -5,32 +5,6 @@ public abstract class IRequest {
     public String URI;
     public String User;
     public String Password;
-    public String version;
-    public String xml;
-
-    public IRequest(String URI, String user, String password, String version, boolean isb64) {
-        this.URI = URI;
-        User = user;
-        Password = password;
-        this.version = version;
-        this.isb64 = isb64;
-    }
-
-    public IRequest(String token, String URI, String version, boolean isb64) {
-
-        Token = token;
-        this.URI = URI;
-        this.version = version;
-        this.isb64 = isb64;
-    }
-
-    public boolean isb64;
-
-    public IRequest(String token, String URI, boolean isb64) {
-        Token = token;
-        this.URI = URI;
-        this.isb64 = isb64;
-    }
 
     public IRequest(String URI, String user, String password) {
         this.URI = URI;
@@ -38,10 +12,8 @@ public abstract class IRequest {
         Password = password;
     }
 
-    public IRequest(String token, String URI, String xml, String version) {
+    public IRequest(String token, String URI) {
         Token = token;
         this.URI = URI;
-        this.version = version;
-        this.xml = xml;
     }
 }

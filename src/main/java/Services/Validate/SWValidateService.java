4e1dfb91a7d8c3e6543375258e45feced2ce061c
Martin Flores <martin.flores@sw.com.mx>
2022-01-20 02:22:12 -0600

    Eliminación lco y lrfc

--- src/main/java/Services/Validate/SWValidateService.java
+++ src/main/java/Services/Validate/SWValidateService.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 import Exceptions.AuthException;
 import Exceptions.GeneralException;
 import Services.SWService;
-import Utils.Requests.Validate.ValidateLcoOptionsRequest;
-import Utils.Requests.Validate.ValidateLcoRequest;
-import Utils.Requests.Validate.ValidateLrfcOptionsRequest;
-import Utils.Requests.Validate.ValidateLrfcRequest;
 import Utils.Requests.Validate.ValidateXmlOptionsRequest;
 import Utils.Requests.Validate.ValidateXmlRequest;
 import Utils.Responses.IResponse;
@@ -37,15 +33,4 @@ public class SWValidateService extends SWService {
 		return req.sendRequest(settings);
 	}
 
-	public IResponse ValidateLrfc(String Lrfc) throws AuthException, GeneralException, IOException {
-		ValidateLrfcOptionsRequest settings = new ValidateLrfcOptionsRequest(getToken(), getURI(), Lrfc, getProxyHost(), getProxyPort());
-		ValidateLrfcRequest req = new ValidateLrfcRequest();
-		return req.sendRequest(settings);
-	}
-
-	public IResponse ValidateLco(String Lco) throws AuthException, GeneralException, IOException {
-		ValidateLcoOptionsRequest settings = new ValidateLcoOptionsRequest(getToken(), getURI(), Lco, getProxyHost(), getProxyPort());
-		ValidateLcoRequest req = new ValidateLcoRequest();
-		return req.sendRequest(settings);
-	}
 }
--- src/test/java/Tests/StatusCfdi/StatusCfdiServiceTest.java
+++ src/test/java/Tests/StatusCfdi/StatusCfdiServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import Services.StatusCfdi.StatusCfdiService;
 import Utils.Responses.StatusCfdi.StatusCfdiResponse;
+import org.junit.Ignore;
 
 public class StatusCfdiServiceTest {
 	@Test
@@ -25,7 +26,7 @@ public class StatusCfdiServiceTest {
 	public void testStatusCancelationService_Test() throws Exception {
 		StatusCfdiService app = new StatusCfdiService("https://pruebacfdiconsultaqr.cloudapp.net/ConsultaCFDIService.svc", "http://tempuri.org/IConsultaCFDIService/Consulta");
 		StatusCfdiResponse response = null;
-		response = (StatusCfdiResponse) app.StatusCfdi("API6609273E0", "XAXX010101000", "314.00", "39292240-74c8-40fa-9192-bdca4b412d95","lWZ1DQ==");
+		response = (StatusCfdiResponse) app.StatusCfdi("EKU9003173C9", "XEXX010101000", "11.48", "3eeb3400-4c53-454c-91c7-caf05a7f111b","kRLGkg==");
 		System.out.println(response.Status);
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.codigoEstatus);

3ce7a8b310a08cb80d2f283ffc434511a40d82df
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-10-04 01:06:27 -0500

    Agregar enum PdfTemplates -Se agrega un enum para las plantillas genericas.

---
+++ src/main/java/Utils/Helpers/PdfTemplates.java
@@ -0,0 +1,38 @@
+package Utils.Helpers;
+/**
+ * Plantilas Genericas
+ */
+public enum PdfTemplates {
+	/**
+	 * CFDI 3.3
+	 */
+	cfdi33,
+	/**
+	 * CFDI 4.0
+	 */
+	cfdi40,
+	/**
+	 * Pagos 1.0
+	 */
+	payment,
+	/**
+	 * Pagos 2.0
+	 */
+	payment20,
+	/**
+	 * Nomina Rev. B
+	 */
+	payroll,
+	/**
+	 * Nomina Rev. C
+	 */
+	payroll40,
+	/**
+	 * Carta Porte 2.0 CFDI 3.3
+	 */
+	billoflading20,
+	/**
+	 * Carta Porte 2.0 CFDI 4.0
+	 */
+	billoflading40
+}

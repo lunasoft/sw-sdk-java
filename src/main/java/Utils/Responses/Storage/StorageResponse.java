a8e16e612aeff946366c389ed6261c98bfa2d673
SW-2017-04\Soporte2 <marifer.mares@sw.com.mx>
2023-01-03 12:39:55 -0600

    Se modifico el JavaDoc

--- src/main/java/Utils/Responses/Storage/StorageResponse.java
+++ src/main/java/Utils/Responses/Storage/StorageResponse.java
@@ -11,8 +11,9 @@ public class StorageResponse extends IResponse {
      * 
      * @param httpStatusCode codigo de respuesta.
      * @param status         status de llamada a API.
-     * @param mesg           mensaje devuelto por API.
-     * @param mesgDetail     detalles mensaje de la API.
+     * @param msg            mensaje devuelto por API.
+     * @param msgDetail      detalles mensaje de la API.
+     * @param dataXml        datos del xml 
      */
     public StorageResponse(int httpStatusCode, String status, String msg, String msgDetail,
             StorageData dataXml) {

36cb099fb000d64ed978f9d129bae562721a5465
ReyesSW <david.reyes@sw.com.mx>
2023-08-29 16:31:32 -0600

    Se homologa a un solo request

--- src/main/java/Utils/Requests/Stamp/StampRequest.java
+++ src/main/java/Utils/Requests/Stamp/StampRequest.java
@@ -69,4 +69,43 @@ public class StampRequest implements IRequestor {
 		}
 
 	}
+	public IResponse sendRequestZip(IRequest request) throws GeneralException,
+            AuthException {
+        try {
+            byte[] zipData = ((StampOptionsRequest) request).getZipData();
+
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpPost httppost = new HttpPost(request.URI);
+
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+            // Agregar el archivo ZIP como parte del contenido multipart
+            builder.addBinaryBody("xml", zipData, ContentType.APPLICATION_OCTET_STREAM,
+                    "zip");
+
+            // Agrega el Bearer token al encabezado "Authorization"
+            httppost.addHeader("Authorization", "Bearer " + request.Token);
+
+            httppost.setEntity(builder.build());
+
+            try (CloseableHttpResponse responseB = client.execute(httppost)) {
+                String responseString = EntityUtils.toString(responseB.getEntity(), "UTF-8");
+                int status = responseB.getStatusLine().getStatusCode();
+        
+                 // Imprimir la respuesta para depurar
+				 System.out.println(responseString);
+
+				 ResponseStamp R = RespuestaTimbrado.Stamped(request.version.charAt(request.version.length() -
+						 1));
+				 R.setReason(responseB.getStatusLine());
+				 R.setResponse(responseString);
+				 R.setStatus(status);
+ 
+				 return R.getResponse();
+            }
+        } catch (IOException e) {
+            throw new GeneralException(500, e.getMessage());
+        }
+    }
 }

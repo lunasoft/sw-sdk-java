e453e80930cbd8ef84a13fb58caba98689908f90
ReyesSW <david.reyes@sw.com.mx>
2023-08-30 16:37:11 -0600

    Se modifica archivo para que no envie un path al servicio si no bytes.

--- src/test/java/Tests/Stamp/SWStampsServiceZipTest.java
+++ src/test/java/Tests/Stamp/SWStampsServiceZipTest.java
@@ -9,13 +9,17 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class SWStampsServiceZipTest {
-    //STANDARD XML
-    @Test
+	// STANDARD XML
+	@Ignore
 	public void testStamp_ZipXML() throws Exception {
-        SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
+		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
 		SuccessV1Response response = null;
-		response = (SuccessV1Response) api.StampZip("src/test/resources/CFDI40/ZIP/120000conceptos.zip","v1");
+		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/120000conceptos.zip"));
+		response = (SuccessV1Response) api.StampZip(zipData, "v1");
 		System.out.println(response.Status);
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.tfd);
@@ -23,16 +27,52 @@ public class SWStampsServiceZipTest {
 		String expect_status = "success";
 		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
 	}
-	@Test
-	public void testStamp_ZipXML_Error() throws Exception {
+
+	@Ignore
+	public void testStamp_ZipXML_NoSellado() throws Exception {
         SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
 		SuccessV1Response response = null;
-		response = (SuccessV1Response) api.StampZip("src/test/resources/CFDI40/ZIP/120000conceptosError.zip","v1");
+		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/120000conceptosError.zip"));
+		response = (SuccessV1Response) api.StampZip(zipData,"v1");
 		System.out.println(response.Status);
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.tfd);
 		System.out.println(response.message);
-		String expect_status = "error";
+		if(response.message == "CFDI40102 - El resultado de la digestión debe ser igual al resultado de la desencripción del sello."){
+			String expect_status = "error";
 		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+		}
+		
+	}
+
+	@Ignore
+	public void testStamp_ZipXML_FueraRango() throws Exception {
+		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
+		SuccessV1Response response = null;
+		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/Mas120000conceptos.zip"));
+		response = (SuccessV1Response) api.StampZip(zipData, "v1");
+		System.out.println(response.Status);
+		System.out.println(response.HttpStatusCode);
+		System.out.println(response.tfd);
+		System.out.println(response.message);
+		if(response.message == "En este path sólo es posible timbrar facturas que tengan entre 110000 y 120000 nodos cfdi:Concepto  por este path"){
+			String expect_status = "error";
+		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+		}
+	}
+	@Test
+	public void testStamp_ZipXML_EstructuraIncorrecta() throws Exception {
+		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
+		SuccessV1Response response = null;
+		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/120000conceptosEstructError.zip"));
+		response = (SuccessV1Response) api.StampZip(zipData, "v1");
+		System.out.println(response.Status);
+		System.out.println(response.HttpStatusCode);
+		System.out.println(response.tfd);
+		System.out.println(response.message);
+		if(response.message == "301 - La estructura del comprobante es incorrecta."){
+			String expect_status = "error";
+		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+		}
 	}
 }

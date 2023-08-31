3c7671d62a82def636564162081b51238a4a9a0e
ReyesSW <david.reyes@sw.com.mx>
2023-08-31 08:16:30 -0600

    Se eliminan comentarios y se identa el codigo.

--- src/main/java/Utils/Requests/Stamp/StampRequest.java
+++ src/main/java/Utils/Requests/Stamp/StampRequest.java
@@ -55,7 +55,7 @@ public class StampRequest implements IRequestor {
 			int status = responseB.getStatusLine().getStatusCode();
 			client.close();
 			responseB.close();
-			ResponseStamp R = RespuestaTimbrado.Stamped(request.version.charAt(request.version.length()-1));
+			ResponseStamp R = RespuestaTimbrado.Stamped(request.version.charAt(request.version.length() - 1));
 			R.setReason(responseB.getStatusLine());
 			R.setResponse(responseString);
 			R.setStatus(status);
@@ -69,43 +69,41 @@ public class StampRequest implements IRequestor {
 		}
 
 	}
+
 	public IResponse sendRequestZip(IRequest request) throws GeneralException,
-            AuthException {
-        try {
-            byte[] zipData = ((StampOptionsRequest) request).getZipData();
+			AuthException {
+		try {
+			byte[] zipData = ((StampOptionsRequest) request).getZipData();
 
-            CloseableHttpClient client = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost(request.URI);
+			CloseableHttpClient client = HttpClients.createDefault();
+			HttpPost httppost = new HttpPost(request.URI);
 
-            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+			builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
-            // Agregar el archivo ZIP como parte del contenido multipart
-            builder.addBinaryBody("xml", zipData, ContentType.APPLICATION_OCTET_STREAM,
-                    "zip");
+			builder.addBinaryBody("xml", zipData, ContentType.APPLICATION_OCTET_STREAM,
+					"zip");
 
-            // Agrega el Bearer token al encabezado "Authorization"
-            httppost.addHeader("Authorization", "Bearer " + request.Token);
+			httppost.addHeader("Authorization", "Bearer " + request.Token);
 
-            httppost.setEntity(builder.build());
+			httppost.setEntity(builder.build());
 
-            try (CloseableHttpResponse responseB = client.execute(httppost)) {
-                String responseString = EntityUtils.toString(responseB.getEntity(), "UTF-8");
-                int status = responseB.getStatusLine().getStatusCode();
-        
-                 // Imprimir la respuesta para depurar
-				 System.out.println(responseString);
+			try (CloseableHttpResponse responseB = client.execute(httppost)) {
+				String responseString = EntityUtils.toString(responseB.getEntity(), "UTF-8");
+				int status = responseB.getStatusLine().getStatusCode();
 
-				 ResponseStamp R = RespuestaTimbrado.Stamped(request.version.charAt(request.version.length() -
-						 1));
-				 R.setReason(responseB.getStatusLine());
-				 R.setResponse(responseString);
-				 R.setStatus(status);
- 
-				 return R.getResponse();
-            }
-        } catch (IOException e) {
-            throw new GeneralException(500, e.getMessage());
-        }
-    }
+				System.out.println(responseString);
+
+				ResponseStamp R = RespuestaTimbrado.Stamped(request.version.charAt(request.version.length() -
+						1));
+				R.setReason(responseB.getStatusLine());
+				R.setResponse(responseString);
+				R.setStatus(status);
+
+				return R.getResponse();
+			}
+		} catch (IOException e) {
+			throw new GeneralException(500, e.getMessage());
+		}
+	}
 }
--- src/test/java/Tests/Stamp/SWStampsServiceZipTest.java
+++ src/test/java/Tests/Stamp/SWStampsServiceZipTest.java
@@ -13,7 +13,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SWStampsServiceZipTest {
-	// STANDARD XML
 	@Test
 	public void testStamp_ZipXML() throws Exception {
 		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
@@ -30,19 +29,19 @@ public class SWStampsServiceZipTest {
 
 	@Test
 	public void testStamp_ZipXML_NoSellado() throws Exception {
-        SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
+		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
 		SuccessV1Response response = null;
 		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/120000conceptosError.zip"));
-		response = (SuccessV1Response) api.StampZip(zipData,"v1");
+		response = (SuccessV1Response) api.StampZip(zipData, "v1");
 		System.out.println(response.Status);
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.tfd);
 		System.out.println(response.message);
-		if(response.message == "CFDI40102 - El resultado de la digestión debe ser igual al resultado de la desencripción del sello."){
+		if (response.message == "CFDI40102 - El resultado de la digestión debe ser igual al resultado de la desencripción del sello."){ 
 			String expect_status = "error";
-		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+			Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
 		}
-		
+
 	}
 
 	@Test
@@ -55,11 +54,12 @@ public class SWStampsServiceZipTest {
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.tfd);
 		System.out.println(response.message);
-		if(response.message == "En este path sólo es posible timbrar facturas que tengan entre 110000 y 120000 nodos cfdi:Concepto  por este path"){
+		if (response.message == "En este path sólo es posible timbrar facturas que tengan entre 110000 y 120000 nodos cfdi:Concepto  por este path"){ 
 			String expect_status = "error";
-		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+			Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
 		}
 	}
+
 	@Test
 	public void testStamp_ZipXML_EstructuraIncorrecta() throws Exception {
 		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
@@ -70,9 +70,9 @@ public class SWStampsServiceZipTest {
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.tfd);
 		System.out.println(response.message);
-		if(response.message == "301 - La estructura del comprobante es incorrecta."){
+		if (response.message == "301 - La estructura del comprobante es incorrecta.") {
 			String expect_status = "error";
-		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+			Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
 		}
 	}
 }

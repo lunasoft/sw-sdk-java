b43434ad7362c58241c7a244858a45457e0a32f1
Sebastian Vidal <sebastian.vidal@sw.com.mx>
2023-12-14 10:31:07 -0600

    Privacy improvement and addition of comments

--- src/main/java/Services/BalanceAccount/SWBalanceAccountService.java
+++ src/main/java/Services/BalanceAccount/SWBalanceAccountService.java
@@ -1,3 +1,7 @@
+/**
+ * SWBalanceAccountService proporciona servicios para gestionar el saldo de cuentas.
+ * Esta clase extiende SWService y ofrece métodos para obtener el saldo de una cuenta y para realizar un movimiento de saldo (agregar y remover).
+ */
 package Services.BalanceAccount;
 
 import java.io.IOException;
@@ -10,37 +14,96 @@ import Utils.Requests.BalanceAccount.BalanceAcctRequest;
 import Utils.Helpers.EnumBalanceStamp.AccountBalanceAction;
 import Utils.Responses.IResponse;
 
-
+/**
+ * SWBalanceAccountService proporciona servicios para gestionar el saldo de
+ * cuentas.
+ */
 public class SWBalanceAccountService extends SWService {
-    
+
+    /**
+     * Constructor que requiere credenciales de autenticación para el servicio.
+     * @param user              Nombre de usuario para la autenticación.
+     * @param password          Contraseña para la autenticación.
+     * @param URI               URI base para la autenticación.
+     * @throws AuthException    Si la autenticación falla.
+     */
     public SWBalanceAccountService(String user, String password, String URI) throws AuthException {
         super(user, password, URI);
     }
 
+    /**
+     * Constructor que utiliza un token de autenticación para el servicio.
+     * @param user              Nombre de usuario para la autenticación.
+     * @param password          Contraseña para la autenticación.
+     * @param URI               URI base para la autenticación.
+     * @param URIAPI            URI de la API para el servicio.
+     * @throws AuthException    Si la autenticación falla.
+     */
     public SWBalanceAccountService(String user, String password, String URI, String URIAPI) throws AuthException {
         super(user, password, URI, URIAPI);
     }
 
-    public SWBalanceAccountService(String token, String URI) {
+    /**
+     * Constructor que utiliza un token de autenticación para el servicio.
+     * @param token             Token de autenticación.
+     * @param URI               URI base para la autenticación.
+     * @throws AuthException    Si la autenticación falla.
+     */
+    public SWBalanceAccountService(String token, String URI) throws AuthException {
         super(token, URI);
     }
-    
+
+    /**
+     * Constructor que utiliza un token de autenticación para el servicio.
+     * @param user             Nombre de usuario para la autenticación.
+     * @param password         Contraseña para la autenticación.
+     * @param URI              URI base para la autenticación.
+     * @param proxyHost        Host del proxy (puede ser nulo).
+     * @param proxyPort        Puerto del proxy.
+     * @throws AuthException    Si la autenticación falla.
+     */
     public SWBalanceAccountService(String user, String password, String URI, String proxyHost, int proxyPort) throws AuthException {
         super(user, password, URI, proxyHost, proxyPort);
     }
 
+    /**
+     * Constructor que utiliza un token de autenticación para el servicio.
+     * @param token             Token de autenticación.
+     * @param URI               URI base para la autenticación.
+     * @param proxyHost         Host del proxy (puede ser nulo).
+     * @param proxyPort         Puerto del proxy.
+     * @throws AuthException    Si la autenticación falla.
+     */
     public SWBalanceAccountService(String token, String URI, String proxyHost, int proxyPort) {
         super(token, URI, proxyHost, proxyPort);
     }
-    
+
+    /**
+     * Obtiene el saldo de la cuenta.
+     * @return                  IResponse con el resultado de la operación.
+     * @throws AuthException    Si la autenticación falla.
+     * @throws GeneralException Si ocurre un error general.
+     * @throws IOException      Si hay un error de entrada/salida.
+     */
     public IResponse GetBalanceAccount() throws AuthException, GeneralException, IOException {
-        BalanceAcctOptionsRequest settings = new BalanceAcctOptionsRequest(getToken(), getURI(), getProxyHost(), getProxyPort());
+        BalanceAcctOptionsRequest settings = BalanceAcctOptionsRequest.sendRequest(getToken(), getURI(), getProxyHost(), getProxyPort());
         BalanceAcctRequest req = new BalanceAcctRequest();
         return req.sendRequest(settings);
     }
 
+    /**
+     * Realiza un movimiento de saldo en la cuenta.
+     * @param idUser            ID del usuario.
+     * @param stamps            Número de timbres a agregar o remover.
+     * @param comment           Comentario del movimiento.
+     * @param action            Tipo de movimiento (add o remove).
+     * @return                  IResponse con el resultado de la operación.
+     * @throws AuthException    Si la autenticación falla.
+     * @throws GeneralException Si ocurre un error general.
+     * @throws IOException      Si hay un error de entrada/salida.
+     */
     public IResponse BalanceAccountStamp(UUID idUser, int stamps, String comment, AccountBalanceAction action) throws AuthException, GeneralException, IOException {
-        BalanceAcctOptionsRequest settings = new BalanceAcctOptionsRequest(getToken(),  getURIAPI() == null ? getURI() : getURIAPI(), idUser, stamps, comment, action, getProxyHost(), getProxyPort());
+        BalanceAcctOptionsRequest settings = BalanceAcctOptionsRequest.BalanceAccountStamp(getToken(), getURIAPI() == null ? getURI() : getURIAPI(), idUser, stamps, comment, action, getProxyHost(), getProxyPort());
         BalanceAcctRequest req = new BalanceAcctRequest();
         return req.BalanceStampRequest(settings);
     }
--- src/main/java/Utils/Requests/BalanceAccount/BalanceAcctOptionsRequest.java
+++ src/main/java/Utils/Requests/BalanceAccount/BalanceAcctOptionsRequest.java
@@ -6,14 +6,65 @@ import Utils.Constants;
 import Utils.Requests.IRequest;
 import Utils.Helpers.EnumBalanceStamp.AccountBalanceAction;
 
+/**
+ * La clase BalanceAcctOptionsRequest representa las opciones de solicitud para
+ * operaciones relacionadas con el saldo de cuentas.
+ * Extiende la clase IRequest para incluir información común de solicitud.
+ */
+
 public class BalanceAcctOptionsRequest extends IRequest {
-    
-    public BalanceAcctOptionsRequest(String token, String URI, String proxyHost, int proxyPort) {
-        super(token, URI+ Constants.BALANCE_ACCOUNT_PATH, proxyHost, proxyPort);
+    private UUID idUser;
+    private int stamps;
+    private String comment;
+    private AccountBalanceAction action;
+
+    /**
+     * Constructor privado para crear una instancia de BalanceAcctOptionsRequest.
+     * Utilizado por los métodos estáticos de creación de solicitudes.
+     */
+    private BalanceAcctOptionsRequest(String token, String URI, String proxyHost, int proxyPort) {
+        super(token, URI + Constants.BALANCE_ACCOUNT_PATH, proxyHost, proxyPort);
     }
 
-    public BalanceAcctOptionsRequest(String token, String URI, UUID idUser, int stamps, String comment, AccountBalanceAction action, String proxyHost, int proxyPort) {
-        super(token, URI+ Constants.BALANCE_ACCOUNT_MANAGEMENT_PATH, idUser, stamps, comment, action, proxyHost, proxyPort);
+    /**
+     * Constructor privado para crear una instancia de BalanceAcctOptionsRequest.
+     * Utilizado por los métodos estáticos de creación de solicitudes.
+     */
+    private BalanceAcctOptionsRequest(String token, String URI, UUID idUser, int stamps, String comment, AccountBalanceAction action, String proxyHost, int proxyPort) {
+        super(token, URI + Constants.BALANCE_ACCOUNT_MANAGEMENT_PATH, idUser, stamps, comment, action, proxyHost, proxyPort);
+        
     }
 
+    /**
+     * Método estático para crear una solicitud de obtención de saldo de cuenta.
+     */
+    public static BalanceAcctOptionsRequest sendRequest(String token, String URI, String proxyHost, int proxyPort) {
+        return new BalanceAcctOptionsRequest(token, URI, proxyHost, proxyPort);
+    }
+
+    /**
+     * Método estático para crear una solicitud de movimiento de saldo de cuenta.
+     */
+    public static BalanceAcctOptionsRequest BalanceAccountStamp(String token, String URI, UUID idUser, int stamps, String comment, AccountBalanceAction action, String proxyHost, int proxyPort) {
+        return new BalanceAcctOptionsRequest(token, URI, idUser, stamps, comment, action, proxyHost, proxyPort);
+    }
+
+    /**
+     * Métodos para obtener información de la solicitud.
+     */
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public int getStamps() {
+        return stamps;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public AccountBalanceAction getAction() {
+        return action;
+    }
 }

e0c760120ac136c23671408b21e107d4b710a515
Maria Fernanda Lopez Mares <108951979+marifersw@users.noreply.github.com>
2023-12-14 00:34:47 -0600

    UTResendService (Exceptions)

---
+++ src/test/java/Tests/Resend/SWResendServiceTest.java
@@ -0,0 +1,96 @@
+package Tests.Resend;
+
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Services.Resend.SWResendService;
+import Tests.Utils;
+import Utils.Responses.Resend.ResendResponse;
+
+public class SWResendServiceTest {
+        @Test
+        public void ResendEmail_Success() throws Exception {
+                SWResendService app = new SWResendService(Utils.userSW, Utils.passwordSW, Utils.urlSW, Utils.urlApiSW,
+                                null,
+                                0);
+                ResendResponse response = null;
+                response = (ResendResponse) app.ResendEmail(UUID.fromString("4714f6f7-ccb4-4eb5-8ba6-3a523092e2b4"),
+                                "ex1@gmail.com,ex2@gmail.com,ex3@gmail.com,ex4@gmail.com,ex5@gmail.com");
+                Assert.assertNotNull(response.HttpStatusCode);
+                Assert.assertNotNull(response.Status);
+                Assert.assertNotNull(response.data);
+                Assert.assertNotNull(response.message);
+                Assert.assertNotNull(response.messageDetail);
+                Assert.assertTrue(response.Status.equals("success"));
+
+        }
+
+        @Test
+        public void ResendEmail_Token_Success() throws Exception {
+                SWResendService app = new SWResendService(Utils.tokenSW, Utils.urlApiSW, null, 0);
+                ResendResponse response = null;
+                response = (ResendResponse) app.ResendEmail(UUID.fromString("4714f6f7-ccb4-4eb5-8ba6-3a523092e2b4"),
+                                "ex1@gmail.com,ex2@gmail.com,ex3@gmail.com,ex4@gmail.com,ex5@gmail.com");
+                Assert.assertNotNull(response.HttpStatusCode);
+                Assert.assertNotNull(response.Status);
+                Assert.assertNotNull(response.data);
+                Assert.assertNotNull(response.message);
+                Assert.assertNotNull(response.messageDetail);
+                Assert.assertTrue(response.Status.equals("success"));
+        }
+
+        @Test
+        public void ResendEmail_Token_Error() throws Exception {
+                SWResendService app = new SWResendService("fakeToken...", Utils.urlApiSW, null, 0);
+                ResendResponse response = null;
+                response = (ResendResponse) app.ResendEmail(UUID.fromString("4714f6f7-ccb4-4eb5-8ba6-3a523092e2b4"),
+                                "ex1@gmail.com,ex2@gmail.com,ex3@gmail.com,ex4@gmail.com,ex5@gmail.com");
+                Assert.assertNotNull(response.HttpStatusCode);
+                Assert.assertNotNull(response.Status);
+                Assert.assertNotNull(response.message);
+                Assert.assertNotNull(response.messageDetail);
+                Assert.assertTrue(response.Status.equals("error"));
+
+        }
+        @Test
+        public void ResendEmail_Emails_Error() throws Exception {
+                SWResendService app = new SWResendService(Utils.tokenSW, Utils.urlApiSW, null, 0);
+                ResendResponse response = null;
+                response = (ResendResponse) app.ResendEmail(UUID.fromString("4714f6f7-ccb4-4eb5-8ba6-3a523092e2b4"),
+                                "ex1@gmail.com,ex2@gmail.com, ex3@gmail.com,ex4@gmail.com,ex5@gmail.com");
+                Assert.assertNotNull(response.HttpStatusCode);
+                Assert.assertNotNull(response.Status);
+                Assert.assertNotNull(response.message);
+                Assert.assertNotNull(response.messageDetail);
+                Assert.assertTrue(response.Status.equals("error"));
+
+        }
+        @Test
+        public void ResendEmail_EmailSize_Error() throws Exception {
+                SWResendService app = new SWResendService(Utils.tokenSW, Utils.urlApiSW, null, 0);
+                ResendResponse response = null;
+                response = (ResendResponse) app.ResendEmail(UUID.fromString("4714f6f7-ccb4-4eb5-8ba6-3a523092e2b4"),
+                                "ex1@gmail.com,ex2@gmail.com,ex3@gmail.com,ex4@gmail.com,ex5@gmail.com,ex6@gmail.com");
+                Assert.assertNotNull(response.HttpStatusCode);
+                Assert.assertNotNull(response.Status);
+                Assert.assertNotNull(response.message);
+                Assert.assertNotNull(response.messageDetail);
+                Assert.assertTrue(response.Status.equals("error"));
+
+        }
+        @Test
+        public void ResendEmail_EmptyEmail_Error() throws Exception {
+                SWResendService app = new SWResendService(Utils.tokenSW, Utils.urlApiSW, null, 0);
+                ResendResponse response = null;
+                response = (ResendResponse) app.ResendEmail(UUID.fromString("4714f6f7-ccb4-4eb5-8ba6-3a523092e2b4"),
+                                "");
+                Assert.assertNotNull(response.HttpStatusCode);
+                Assert.assertNotNull(response.Status);
+                Assert.assertNotNull(response.message);
+                Assert.assertNotNull(response.messageDetail);
+                Assert.assertTrue(response.Status.equals("error"));
+
+        }
+}

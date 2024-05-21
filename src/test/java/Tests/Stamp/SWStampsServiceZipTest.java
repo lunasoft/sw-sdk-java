44b790b93474ec691089b5719f77cd1191a82fef
Fernando Carrillo <108951984+FerCarrilloM@users.noreply.github.com>
2024-05-20 18:36:28 -0600

    Se ajustan UT para ejemplos de XML largos.

--- src/test/java/Tests/Stamp/SWStampsServiceZipTest.java
+++ src/test/java/Tests/Stamp/SWStampsServiceZipTest.java
@@ -3,6 +3,7 @@ package Tests.Stamp;
 import Services.Stamp.SWStampService;
 import Tests.Utils;
 import Utils.Responses.Stamp.SuccessV1Response;
+
 import org.junit.Assert;
 import org.junit.Test;
 import java.nio.charset.StandardCharsets;
@@ -16,11 +17,26 @@ public class SWStampsServiceZipTest {
 		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
 		SuccessV1Response response = null;
 		Utils ut = new Utils();
-		String xml = ut.StringgenZip(false);
+		String xml = ut.StringgenLongXML(false);
 		byte[] byteXML = xml.getBytes(StandardCharsets.UTF_8);
 		response = (SuccessV1Response) api.Stamp(byteXML, "v1");
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.message);
+		System.out.println(response.messageDetail);
+		String expect_status = "success";
+		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
+		Assert.assertTrue("El campo tfd no debe estar vacío", response.tfd != null && !response.tfd.trim().isEmpty());
+	}
+	@Test
+	public void testStamp_ZipXML_String() throws Exception {
+		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
+		SuccessV1Response response = null;
+		Utils ut = new Utils();
+		String xml = ut.StringgenLongXML(false);
+		response = (SuccessV1Response) api.Stamp(xml, "v1");
+		System.out.println(response.HttpStatusCode);
+		System.out.println(response.message);
+		System.out.println(response.messageDetail);
 		String expect_status = "success";
 		Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
 		Assert.assertTrue("El campo tfd no debe estar vacío", response.tfd != null && !response.tfd.trim().isEmpty());
@@ -30,7 +46,7 @@ public class SWStampsServiceZipTest {
 	public void testStamp_ZipXML_NoSellado() throws Exception {
 		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
 		SuccessV1Response response = null;
-		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/120000conceptosError.xml"));
+		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/155000conceptosError.xml"));
 		response = (SuccessV1Response) api.Stamp(zipData, "v1");
 		System.out.println(response.Status);
 		System.out.println(response.HttpStatusCode);
@@ -46,29 +62,13 @@ public class SWStampsServiceZipTest {
 	public void testStamp_ZipXML_EstructuraIncorrecta() throws Exception {
 		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
 		SuccessV1Response response = null;
-		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/120000conceptosEstructError.xml"));
+		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/155000conceptosEstructError.xml"));
 		response = (SuccessV1Response) api.Stamp(zipData, "v1");
 		System.out.println(response.Status);
 		System.out.println(response.HttpStatusCode);
 		System.out.println(response.tfd);
 		System.out.println(response.message);
 		if (response.message == "301 - La estructura del comprobante es incorrecta.") {
-			String expect_status = "error";
-			Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
-		}
-	}
-
-	@Test
-	public void testStamp_ZipXML_FueraRango() throws Exception {
-		SWStampService api = new SWStampService(Utils.userSW, Utils.passwordSW, Utils.urlSW);
-		SuccessV1Response response = null;
-		byte[] zipData = Files.readAllBytes(Paths.get("src/test/resources/CFDI40/ZIP/Mas120000conceptos.xml"));
-		response = (SuccessV1Response) api.Stamp(zipData, "v1");
-		System.out.println(response.Status);
-		System.out.println(response.HttpStatusCode);
-		System.out.println(response.tfd);
-		System.out.println(response.message);
-		if (response.message == "En este path sólo es posible timbrar facturas que tengan entre 110000 y 120000 nodos cfdi:Concepto  por este path"){ 
 			String expect_status = "error";
 			Assert.assertTrue(expect_status.equalsIgnoreCase(response.Status));
 		}
